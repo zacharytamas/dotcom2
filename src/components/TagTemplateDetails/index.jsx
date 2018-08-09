9322f3d79d8a7a52eb26b90cd63ab932913fa058
Zachary Jones <zachary@thorough.company>
2018-08-09 09:31:23 -0400

    fix: Change wording on tag pages

--- src/components/TagTemplateDetails/index.jsx
+++ src/components/TagTemplateDetails/index.jsx
@@ -15,9 +15,7 @@ class TagTemplateDetails extends React.Component {
         <div className="content__inner">
           <div className="page">
             <h1 className="page__title">
-              All Posts tagged as &quot;
-              {tagTitle}
-              &quot;
+              All Posts tagged with "{tagTitle}"
             </h1>
             <div className="page__body">{items}</div>
           </div>
